2f8e6d6e3df084fb5227ca8e42c9849ad0048957
SBurlik <sburlikjr@gmail.com>
2022-04-13 17:21:16 +0300

    Practical4 started, Exercise1&2 done

---
+++ src/lv/tsi/practicalAssignments/Practical_FOUR/EX1/hello/src/main/java/EX1/Greeting.java
@@ -0,0 +1,17 @@
+package EX1;
+
+public class Greeting
+{
+    String name;
+
+    public Greeting(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "content: Hello, my name is " + name + "!";
+    }
+}
---
+++ src/lv/tsi/practicalAssignments/Practical_FOUR/EX1/hello/src/main/java/EX1/GreetingController.java
@@ -0,0 +1,19 @@
+package EX1;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GreetingController
+{
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(defaultValue = "Spring") String name)
+    {
+        Greeting greet = new Greeting(name);
+        return greet.toString();
+    }
+
+}
